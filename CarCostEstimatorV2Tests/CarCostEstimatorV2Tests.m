14a1169fb568e527fd6658370a0e332e31a67c58
masterchief117 <bstewart@Michaels-Mac-mini-2.local>
2013-08-01 13:14:10 -0700

    Initial Commit

---
+++ CarCostEstimatorV2Tests/CarCostEstimatorV2Tests.m
@@ -0,0 +1,36 @@
+//
+//  CarCostEstimatorV2Tests.m
+//  CarCostEstimatorV2Tests
+//
+//  Created by Brent Stewart on 8/1/13.
+//  Copyright (c) 2013 Catalyst It Services. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CarCostEstimatorV2Tests : XCTestCase
+
+@end
+
+@implementation CarCostEstimatorV2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
